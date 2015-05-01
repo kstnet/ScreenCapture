76acc7d302c2fea034787a60b693453d69b85c1a
たねっと <@koheis_vaio>
2015-05-02 03:46:37 +0900

    utilのTimerを使うように修正

--- src/main/ScreenCaptureTest.java
+++ src/main/ScreenCaptureTest.java
@@ -5,44 +5,34 @@ import java.awt.HeadlessException;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.imageio.ImageIO;
-import javax.swing.Timer;
 
-public class ScreenCaptureTest implements ActionListener {
+public class ScreenCaptureTest {
+	public static void main(String[] args) {
+		Timer timer = new Timer();
+		timer.schedule(new CaptureTask(), 1000, 43200000);
+		System.out.println("timer start");
+	}
+}
+
+class CaptureTask extends TimerTask {
 	final static String FILENAME = "ScreenCapture";
 	static short CAPNUM = 0;
-	public static void main(String[] args) {
-		new ScreenCaptureTest();
 
-		while(true){
-
-		}
-	}
-
-	ScreenCaptureTest(){
-		Timer timer;
-		timer = new Timer(43200, this);
-
-		System.out.println("timer start");
-		timer.start();
-	}
-
-	public void actionPerformed(ActionEvent e) {
-		CAPNUM++;
-
-		System.out.println("doCaptureメソッド呼び出し");
-		doCapture();
-	}
-
-	private static void doCapture(){
+	public void run(){
 		BufferedImage image;
+
+		CAPNUM++;
+//		System.out.println("画面キャプチャ実行");
+
 		try {
+			System.out.println("画面キャプチャ実行");
 			image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
 			ImageIO.write(image, "png", new File(FILENAME + CAPNUM + ".png"));
 		} catch (HeadlessException e) {
